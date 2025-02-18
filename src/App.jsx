9a903f2c8348e612bfb920e6f7cc72a5d2f946c5
Jaunty <jaunty5899@gmail.com>
2025-02-18 13:07:35 +0530

    weather api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState, useEffect } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const apiLink =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/kerala?unitGroup=us&key=VCLHNDNGS2Y2HDLJA2VYWUMUC&contentType=json";
+  const fetchApi = async () => {
+    const response = await fetch(apiLink);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  };
+  fetchApi();
   return <div className="container"></div>;
 }
 
