e2dc815a35ad884ba94e5cc3aa60341a93bd6e2a
Jaunty <jaunty5899@gmail.com>
2025-02-24 16:54:21 +0530

    chart added

--- src/ForecastChart.jsx
+++ src/ForecastChart.jsx
@@ -2,7 +2,7 @@ import "./ForecastChart.css";
 import { LineChart } from "@mui/x-charts/LineChart";
 
 export default function ForecastChart({ daily }) {
-  const dailyTemps = daily.map((day) => Math.floor(day.temp));
+  const dailyTemps = daily.map((day) => day.temp);
   const dailyLabels = daily.map((day) => day.datetime);
   console.log(dailyTemps, dailyLabels);
 
@@ -14,15 +14,11 @@ export default function ForecastChart({ daily }) {
           xAxis={[
             { data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] },
           ]}
-          series={[
-            {
-              data: dailyTemps,
-            },
-          ]}
+          series={[{ curve: "linear", data: dailyTemps }]}
           width={1140}
-          height={200}
+          height={180}
           margin={{ left: 30, top: 20 }}
-          colors={["white"]}
+          colors={["black"]}
         />
       </div>
     </div>
